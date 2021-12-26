1a89014e7966e5a79cb6f297b00bf2865d356f66
Abdullah-yasir <56272322+Abdullah-yasir@users.noreply.github.com>
2021-12-26 16:45:25 +0500

    add documentation

--- util.js
+++ util.js
@@ -1,4 +1,16 @@
+const { PgormError } = require("./errors");
+
+const typeErrors = {
+  string: 'Must be a string',
+  number: 'Must be a number',
+  function: 'Must be a function',
+  object: 'Must be a object',
+  boolean: 'Must be a boolean',
+  array: 'Must be a array',
+}
+
 module.exports.catchAsync = (fn) => (...args) => {
+  if (typeof fn === '')
     try {
       fn(...args)
     } catch (err) {
@@ -10,3 +22,23 @@ module.exports.catchAsync = (fn) => (...args) => {
 
 module.exports.getTimestamp = () =>
   new Date().toISOString().slice(0, 19).replace("T", " ");
+
+module.exports.isArray = (param) => {
+  if (typeof param === 'object' && param.length) return true;
+  return false;
+}
+
+module.exports.verifyParamType = (paramVal, type, paramName, methodName) => {
+  if (typeof paramVal !== type) throw new PgormError(`'${paramName}' ${typeErrors[type]}`, methodName)
+}
+
+module.exports.verifyParams = (paramsObj, methodName) => {
+  if (paramsObj.length) {
+    // array is provided
+    for ([key, value] of Object.entries(paramsObj)) {
+
+    }
+  } else {
+
+  }
+}
